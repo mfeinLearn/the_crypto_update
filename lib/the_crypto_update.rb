a3b9dd1a22ef814c4d16c3900efffbcce879c8ee
github username <github email address>
2018-11-30 20:43:43 -0500

    puts-ing something out when user runs ./bin/the-crypto-update on top of file

--- lib/the_crypto_update.rb
+++ lib/the_crypto_update.rb
@@ -1,6 +1,6 @@
-require "the_crypto_update/version"
+require_relative "./the_crypto_update/version"
+require_relative './the_crypto_update/cli'
 
-module TheCryptoUpdate
-  class Error < StandardError; end
-  # Your code goes here...
-end
+#the envirnment like file - a single file or anything that
+# I am loading can requrie and this file requires all of my other dependencies
+#16:30
---
+++ lib/the_crypto_update/cli.rb
@@ -0,0 +1,9 @@
+# our cli controller - business logic or user interactions/ dealing with input
+
+class TheCryptoUpdate::CLI
+
+  def call
+    puts "Todays crypto update:"
+  end
+
+end
