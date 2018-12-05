5638ca9168188173833bbd3699e363f2e10226ec
github username <github email address>
2018-12-05 17:38:39 -0500

    modified cli and readme

--- lib/the_crypto_update/cli.rb
+++ lib/the_crypto_update/cli.rb
@@ -7,10 +7,30 @@ class TheCryptoUpdate::CLI
     list_top_coins
     menu
     # my_watch_list
+    details
     close_cli_app
   end
 
   def list_top_coins
+    # here doc - http://blog.jayfields.com/2006/12/ruby-multiline-strings-here-doc-or.html
+    puts ""
+    puts "Todays crypto update:"
+    puts "Volume is the amount of the coin that has been traded with in a certain time frame...fyi"
+    puts "-----------------"
+    puts "|  Coin (Symbol) |"
+    puts "-----------------"
+
+    TheCryptoUpdate::Coin.scrape_binance
+
+    @coins = TheCryptoUpdate::Coin.all[0..99]
+    @coins.each.with_index(1) do |coin, i|
+      # puts "#{i}. #{coin.name} (#{coin.symbol}) | $#{coin.volume_1d} | #{coin.volume_7d}% | #{coin.volume_30d}% |"
+      puts "#{i}. #{coin.name} (#{coin.symbol})"
+
+    end
+  end
+
+  def details
     # here doc - http://blog.jayfields.com/2006/12/ruby-multiline-strings-here-doc-or.html
     puts ""
     puts "Todays crypto update:"
@@ -22,8 +42,8 @@ class TheCryptoUpdate::CLI
     TheCryptoUpdate::Coin.scrape_binance
 
     @coins = TheCryptoUpdate::Coin.all[0..99]
-    @coins.each.with_index(1) do |coin, i|
-      puts "#{i}. #{coin.name} (#{coin.symbol}) | $#{coin.volume_1d} | #{coin.volume_7d}% | #{coin.volume_30d}% |"
+    @coins.find do |coin|
+      puts " #{coin.name} (#{coin.symbol}) | $#{coin.volume_1d} | #{coin.volume_7d}% | #{coin.volume_30d}% |"
     end
   end
 
@@ -40,9 +60,11 @@ class TheCryptoUpdate::CLI
 
         if input.to_i > 0
           the_coin = @coins[input.to_i-1]
-          puts "#{coin.name} (#{coin.symbol}) | $#{coin.volume_1d} | #{coin.volume_7d}% | #{coin.volume_30d}% |"
+          puts "#{the_coin.name} (#{the_coin.symbol}) | $#{the_coin.volume_1d} | #{the_coin.volume_7d}% | #{the_coin.volume_30d}% |"
         elsif input == "list"
           list_top_coins
+        elsif input == "details"
+          details
         else
           puts ""
           puts "Not sure what that means, please type list or exit"
