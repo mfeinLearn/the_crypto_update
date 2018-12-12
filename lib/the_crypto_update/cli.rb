ab2f28e1e9d31904874362865010626fbac960bc
github username <github email address>
2018-12-12 12:55:17 -0500

    addapted for video v.1

--- lib/the_crypto_update/cli.rb
+++ lib/the_crypto_update/cli.rb
@@ -1,6 +1,9 @@
 # our cli controller - business logic or user interactions/ dealing with input
 # require 'colorize'
-#require 'pry'
+require 'pry'
+
+# TheCryptoUpdate - namespace
+# CLI - the class name is CLI
 class TheCryptoUpdate::CLI
 
   # #call - instance method
@@ -9,8 +12,9 @@ class TheCryptoUpdate::CLI
   # 2. menu
   # 3. details
   # 4. close_cli_app
-  def call
+  def call # the #call instance method is called in bin/the-crypto-update file
 
+    ## helper methods
     list_top_coins
     menu
     details
@@ -19,6 +23,9 @@ class TheCryptoUpdate::CLI
     close_cli_app
   end
 
+  # #list_top_coins - instance method - gets 100 of the top coins with their attributes: name and symbol
+  # expected result - to call the coin class and also call the all class method with all of the coins stored and get
+  #   100 of those coins and store them in the instance varable @coins to be used to out-puted in the terminal
   def list_top_coins
     # here doc - http://blog.jayfields.com/2006/12/ruby-multiline-strings-here-doc-or.html
   puts <<-DOC
@@ -36,16 +43,72 @@ class TheCryptoUpdate::CLI
     puts "|  Coin (Symbol) |"
     puts "-----------------"
 
-    TheCryptoUpdate::Coin.scrape_binance
-
+    TheCryptoUpdate::Coin.scrape_coinmarketcap
     @coins = TheCryptoUpdate::Coin.all[0..99]
     @coins.each.with_index(1) do |coin, i|
       # puts "#{i}. #{coin.name} (#{coin.symbol}) | $#{coin.volume_1d} | #{coin.volume_7d}% | #{coin.volume_30d}% |"
       puts "#{i}. #{coin.name} (#{coin.symbol})"
 
     end
+
+    # test 1
+    # puts <<-DOC.gsub /^\s*/, ''
+    #
+    # 1. Bitcoin (BTC)
+    # 2. Tether (USDT)
+    # 3. Ethereum (ETH)
+    # 4. EOS (EOS)
+    # 5. XRP (XRP)
+    # 6. Litecoin (LTC)
+    # 7. Bitcoin Cash (BCH)
+    # 8. Ethereum Cla... (ETC)
+    # 9. Dash (DASH)
+    # 10. Qtum (QTUM)
+    #
+    # DOC
+
+    #test 2
+    # coin_1 = TheCryptoUpdate::CLI.new
+    # coin_1.name = "Bitcoin"
+    # coin_1.symbol = "BTC"
+    # coin_1.volume_1d = "4040.63"
+    # coin_1.volume_7d = "-4.01"
+    # coin_1.volume_30d = "https://info.binance.com/en"
+    #
+    # coin_2 = TheCryptoUpdate::CLI.new
+    # coin_2.name = "Ripple"
+    # coin_2.symbol = "XRP"
+    # coin_2.volume_1d = "0.364702"
+    # coin_2.volume_7d = "-4.01"
+    # coin_2.volume_30d = "https://info.binance.com/en"
+    #
+    # coin_3= TheCryptoUpdate::CLI.new
+    # coin_3.name = "Steller Lumens"
+    # coin_3.symbol = "XLM"
+    # coin_3.volume_1d = "0.159849"
+    # coin_3.volume_7d = "1.23"
+    # coin_3.volume_30d = "https://info.binance.com/en"
+    #
+    # coin_4= TheCryptoUpdate::CLI.new
+    # coin_4.name = "Bitcoin Coin ABC"
+    # coin_4.symbol = "BCHABC"
+    # coin_4.volume_1d = "173.65"
+    # coin_4.volume_7d = "-1.31"
+    # coin_4.volume_30d = "https://info.binance.com/en"
+    #
+    # coin_5= TheCryptoUpdate::CLI.new
+    # coin_5.name = "EOS"
+    # coin_5.symbol = "EOS"
+    # coin_5.volume_1d = "2.88"
+    # coin_5.volume_7d = "-1.73"
+    # coin_5.volume_30d = "https://info.binance.com/en"
+    #
+    # [coin_1.name, coin_2.name, coin_3.name, coin_4.name, coin_5.name]
+
+    #binding.pry
   end
 
+  # #help - is an instance method to help users know what commands are availible to them
   def help
     puts ""
     puts "TYPE ONE OF THE FOLLOWING..."
@@ -61,6 +124,12 @@ class TheCryptoUpdate::CLI
     puts "========================================================="
   end
 
+
+  # #details - instance method - that gets 100 of the top coins with their attributes:
+  # name, symbol, Volume (1 day), Volume (7 day), Volume (30 day)
+  #
+  # expected result - to call the coin class and also call the all class method with all of the coins stored and get
+  #   100 of those coins and store them in the instance varable @coins to be used to out-puted in the terminal
   def details
     # here doc - http://blog.jayfields.com/2006/12/ruby-multiline-strings-here-doc-or.html
     puts ""
@@ -71,15 +140,74 @@ class TheCryptoUpdate::CLI
     puts "----------------------------------------------------------------------"
     puts ""
 
-    TheCryptoUpdate::Coin.scrape_binance
+    TheCryptoUpdate::Coin.scrape_coinmarketcap
 
     @coins = TheCryptoUpdate::Coin.all[0..99]
     @coins.find do |coin|
-      puts " #{coin.name} (#{coin.symbol}) | $#{coin.volume_1d} | #{coin.volume_7d}% | #{coin.volume_30d}% |"
+      puts " #{coin.name} (#{coin.symbol}) | $#{coin.volume_1d} | $#{coin.volume_7d} | $#{coin.volume_30d} |"
     end
-  end
 
+    # test 1
+    # puts <<-DOC.gsub /^\s*/, ''
+    # Bitcoin (BTC) | $4,775,342,769 | $32,509,066,104 | $164,810,366,381 |
+    # Tether (USDT) | $3,108,760,862 | $21,361,843,844 | $111,105,564,842 |
+    # Ethereum (ETH) | $1,679,283,441 | $11,924,292,214 | $61,394,906,973 |
+    # EOS (EOS) | $796,768,352 | $5,021,866,952 | $25,967,872,275 |
+    # XRP (XRP) | $411,218,718 | $2,867,509,194 | $19,814,324,663 |
+    # Litecoin (LTC) | $394,593,514 | $2,741,941,403 | $13,521,000,779 |
+    # Bitcoin Cash (BCH) | $63,654,091 | $713,122,140 | $7,111,621,185 |
+    # Ethereum Cla... (ETC) | $111,782,348 | $953,437,947 | $5,485,805,072 |
+    # Dash (DASH) | $125,256,678 | $973,504,100 | $4,825,876,751 |
+    # Qtum (QTUM) | $116,716,202 | $839,504,120 | $4,747,418,927 |
+    #
+    # DOC
+
+    # test 2
+    # coin_1 = TheCryptoUpdate::Coin.new
+    # coin_1.name = "Bitcoin"
+    # coin_1.symbol = "BTC"
+    # coin_1.price = "4040.63"
+    # coin_1.twemty_four_hr_change = "-4.01"
+    # coin_1.url = "https://info.binance.com/en"
+    #
+    # coin_2 = TheCryptoUpdate::Coin.new
+    # coin_2.name = "Ripple"
+    # coin_2.symbol = "XRP"
+    # coin_2.price = "0.364702"
+    # coin_2.twemty_four_hr_change = "-4.01"
+    # coin_2.url = "https://info.binance.com/en"
+    #
+    # coin_3= TheCryptoUpdate::Coin.new
+    # coin_3.name = "Steller Lumens"
+    # coin_3.symbol = "XLM"
+    # coin_3.price = "0.159849"
+    # coin_3.twemty_four_hr_change = "1.23"
+    # coin_3.url = "https://info.binance.com/en"
+    #
+    # coin_4= TheCryptoUpdate::Coin.new
+    # coin_4.name = "Bitcoin Coin ABC"
+    # coin_4.symbol = "BCHABC"
+    # coin_4.price = "173.65"
+    # coin_4.twemty_four_hr_change = "-1.31"
+    # coin_4.url = "https://info.binance.com/en"
+    #
+    # coin_5= TheCryptoUpdate::Coin.new
+    # coin_5.name = "EOS"
+    # coin_5.symbol = "EOS"
+    # coin_5.price = "2.88"
+    # coin_5.twemty_four_hr_change = "-1.73"
+    # coin_5.url = "https://info.binance.com/en"
+    #
+    # [coin_1.name, coin_2.name, coin_3.name, coin_4.name, coin_5.name]
+
+
+  end
+  # menu - gets user input and direct the user to different methods depending on the input
+  # expected - when the user types a word it is then checked to see if it is one of the options
+  # if it is not one of the options then it should tell the user that it has
+  # no idea what that word mean and to type list or exit
   def menu
+
     puts ""
     # puts "----------------------".colorize(:green)
     # puts "  github:".colorize(:light_blue)
@@ -92,7 +220,7 @@ class TheCryptoUpdate::CLI
 
         if input.to_i > 0
           the_coin = @coins[input.to_i-1]
-          puts "#{the_coin.name} (#{the_coin.symbol}) | $#{the_coin.volume_1d} | #{the_coin.volume_7d}% | #{the_coin.volume_30d}% |"
+          puts "#{the_coin.name} (#{the_coin.symbol}) | $#{the_coin.volume_1d} | $#{the_coin.volume_7d} | $#{the_coin.volume_30d} |"
         elsif input == "help"
           help
         elsif input == "list"
@@ -105,6 +233,27 @@ class TheCryptoUpdate::CLI
           puts ""
         end
       end
+
+    # test 1
+    # puts ""
+    #   input = nil
+    #   while input != "exit"
+    #     puts "Enter the number of the deal you'd like more info on or type list"
+    #     input = gets.strip.downcase
+    #     case input
+    #     when "1"
+    #       puts "Bitcoin (BTC) | $4,775,342,769 | $32,509,066,104 | $164,810,366,381 |"
+    #     when "2"
+    #       puts "Tether (USDT) | $3,108,760,862 | $21,361,843,844 | $111,105,564,842 |"
+    #     when "list"
+    #       list_top_coins
+    #     else
+    #       puts ""
+    #       puts "Not sure what that means, please type list or exit"
+    #       puts ""
+    #     end
+    #   end
+
   end
 
   # def my_watch_list
@@ -118,6 +267,7 @@ class TheCryptoUpdate::CLI
     puts ""
     puts ""
   end
+
 
 
 end
--- lib/the_crypto_update/coin.rb
+++ lib/the_crypto_update/coin.rb
@@ -3,31 +3,41 @@ require 'nokogiri'
 require 'pry'
 
 class TheCryptoUpdate::Coin
+  # attr_accessor - these are methods "setters" and "getters"
   attr_accessor :name, :symbol, :volume_1d, :volume_7d, :volume_30d
 
+  # class method - is an array of Coins instances
   @@all = []
 
+  # this is called when a new coin is created
+  # when this method is called it should automatically check if the key :name is not nil
+  # if it is not nil then take the hash that is passed into the method and set the attributes
+  # to ______________________________________
   def initialize(coin_hash)
     #binding.pry
+      #
       if coin_hash[:name] != nil
       coin_hash.each {|key, value| self.send(("#{key}="), value) }
       @@all << self
     end
   end
 
+  # self.all - this access the @@all (class varable) array that stores all of the coins
   def self.all
     @@all
   end
 
+  # self.current - this calls the self.scrape_coins method
   def self.current
-    # Scrape binance and then return coins based on that data
+    # Scrape coinmarketcap and then return coins based on that data
     self.scrape_coins
   end
 
-  def self.scrape_coins
+  def self.scrape_coins # class method - # this calls the self.scrape_coinmarketcap(class method) and shovel the return value of
+                        # self.scrape_coinmarketcap into a coins array then calling the that coins array
     coins = []
 
-    coins << self.scrape_binance
+    coins << self.scrape_coinmarketcap
 
     # Go to binance, find the coin
     # extract the properties
@@ -35,14 +45,20 @@ class TheCryptoUpdate::Coin
 
     coins
   end
-
-  def self.scrape_binance
-
+  # self.scrape_coinmarketcap - this is where all of the scrapping occures!
+  # self.scrape_coinmarketcap - class method
+  def self.scrape_coinmarketcap
+    # a_coin_obj - Creating a new TheCryptoUpdate::Coin object that takes in a name, symbol, volume_1d, volume_7d, volume_30d
     a_coin_obj = self.new(name: nil, symbol: nil, volume_1d: nil,volume_7d: nil, volume_30d: nil)
 
     coin_attributes = []
+    # doc = uses nokogiri ______________________________________
     doc = Nokogiri::HTML(open("https://coinmarketcap.com/currencies/volume/monthly/"))
 
+    # gets the result of nokogiri and scraps the coinmarketcap.com website
+    # The scrapped data is set to the following varables: name, symbol, volume_1d, volume_7d, volume_30d
+    # The scrapped data varable's are set to keys when a new instance of TheCryptoUpdate::Coin is created this
+    # new object is set to a new varable a_coin_obj
     doc.css("div").css("tbody").css("tr").each do |coin|
     #  binding.pry
       name = coin.css("a").text.split("$")[0]
@@ -62,60 +78,3 @@ class TheCryptoUpdate::Coin
 #binding.pry
   end
 end
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-      # a_coin_obj = self.new(name: name,symbol: symbol, volume_1d: volume_1d, volume_7d: volume_7d,volume_30d: volume_30d)
-    # coin_attributes << {name: symbol, symbol: name, price: price, twemty_four_hr_change: twemty_four_hr_change, url: url}
-
-# name = coin.css("tr#id-bitcoin a.link-secondary").text # name
-# symbol = coin.css("tr#id-bitcoin td.text-left").text
-# # volume_1d = coin.css("tbody.row a.link-secondary").text
-# volume_1d = coin.css("td.text-right")[0].text.strip
-# volume_7d = coin.css("td.text-right")[1].text.strip
-# volume_30d = coin.css("td.text-right")[2].text.strip
-
-
-#doc.css("tbody.s1apzr5v-2.ixZYaO div.name").text # collection of coins
-#doc.css("tbody.s1apzr5v-2.ixZYaO span.abbr")[0].text # symbol
-#doc.css("tbody.s1apzr5v-2.ixZYaO span.fullName")[0].text # name
-#doc.css("div.s1oak5r5-1.hvtvia")[0].text # price
-#doc.css("div.cw0nen-1.hUhTmi")[0].text # twemty_four_hr_change
-
-
-# doc.css("tbody").children.map {|something| something.text}.compact
-
-# doc.css("div").css("tr")[3]
-
-# [17] pry(TheCryptoUpdate::Coin)> doc.css("div").css("tr")[1].css("a").text
-# => "Bitcoin$5,390,905,206$34,204,902,516$157,746,142,836"
-
-# [6] pry(TheCryptoUpdate::Coin)> doc.css("div").css("tr")[0].text
-# => "\n#\nName\nSymbol\nVolume (1d)\nVolume (7d)\nVolume (30d)\n"
-# [7] pry(TheCryptoUpdate::Coin)> doc.css("div").css("tr")[1].text
-# => "\n\n1\n\n\n\nBitcoin\n\nBTC\n\n$5,390,905,206\n\n\n$34,204,902,516\n\n\n$157,746,142,836\n\n"
-# [8] pry(TheCryptoUpdate::Coin)> doc.css("div").css("tr")[2].text
-# => "\n\n2\n\n\n\nTether\n\nUSDT\n\n$3,332,443,355\n\n\n$21,733,057,647\n\n\n$106,856,174,703\n\n"
-# [9] pry(TheCryptoUpdate::Coin)> doc.css("div").css("tr")[3].text
-# => "\n\n3\n\n\n\nEthereum\n\nETH\n\n$1,875,181,858\n\n\n$12,066,793,281\n\n\n$59,030,390,209\n\n"
-# [10] pry(TheCryptoUpdate::Coin)> doc.css("div").css("tr")[4].text
-# => "\n\n4\n\n\n\nEOS\n\nEOS\n\n$841,396,473\n\n\n$4,953,000,504\n\n\n$25,043,775,224\n\n"
-
-# doc.css("div").css("tr")[1].css("a").first
-
-
-# good - doc.css("div").css("tr")[2].css("a").text.split("$")
